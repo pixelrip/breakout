501148d7ba5dc07517ae5c891f802e7a6fbecb76
pixelrip <go@pixel.rip>
2025-10-31 14:07:27 -0500

    Improve collision detection for moving lines
    Updated circle_vs_line to interpolate line endpoints at collision time, ensuring accurate collision checks when both the circle and line are moving. This prevents missed collisions due to line movement between frames.

--- src/collision.lua
+++ src/collision.lua
@@ -3,7 +3,9 @@
 function circle_vs_line(c, l)
     local prev_c = c.prev
     local prev_l = l.prev
-    local p_ly = get_line_y_at_ball(prev_c, l)
+
+    -- Use previous frame's line position for previous y calculation
+    local p_ly = get_line_y_at_ball(prev_c, prev_l)
     local c_ly = get_line_y_at_ball(c, l)
 
     if prev_c.bottom <= p_ly and c.bottom >= c_ly then
@@ -15,14 +17,18 @@ function circle_vs_line(c, l)
         local hit_x = prev_c.x + (c.x - prev_c.x) * t
         local hit_y = prev_c.y + (c.y - prev_c.y) * t
 
-         -- was collision point within line bounds?
-        if hit_x >= l.x1 and hit_x <= l.x2 then
+        -- interpolate line endpoints at collision time t
+        local line_x1_at_t = prev_l.x1 + (l.x1 - prev_l.x1) * t
+        local line_x2_at_t = prev_l.x2 + (l.x2 - prev_l.x2) * t
+
+        -- was collision point within interpolated line bounds?
+        if hit_x >= line_x1_at_t and hit_x <= line_x2_at_t then
             return true, {x = hit_x, y = hit_y, t = t}
         end
     end
 
     -- TODO: Avoid tunneling when moving slowly (e.g., buffer zone)
-    
+
 
     return false
 end
